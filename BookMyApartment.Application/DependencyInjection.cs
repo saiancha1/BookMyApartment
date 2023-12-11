ff3f39f99af51343a9bb638fc7f51197571c2081
Sai Ancha <sai.ancha@cin7.com>
2023-12-11 15:11:12 +1300

    add Application project and DI

---
+++ BookMyApartment.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using BookMyApartment.Domain.Bookings;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookMyApartment.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+        });
+        services.AddTransient<PricingService>();
+        return services;
+    }
+}
